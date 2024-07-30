d0feb94bcdd3e10e636320581988248df6ada903
Talha Sheikh <167472327+Sheikh-talha01@users.noreply.github.com>
2024-07-31 00:24:30 +0500

    Update Code.py

--- Diabetes_Prediction_Model/Code.py
+++ Diabetes_Prediction_Model/Code.py
@@ -1,1 +1,10 @@
 # This repo contains all the code of Diabetes Prediction Model
+import pandas as pd
+import numpy as np
+import sklearn.datasets
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+import seaborn as sns
